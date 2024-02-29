673fb1d51d19abe203528f4f7426877868ba2421
Menesex <158521928+Menesex@users.noreply.github.com>
2024-02-29 18:03:23 -0500

    3 INCOMPLETO

---
+++ 3Taller for-while.py
@@ -0,0 +1,29 @@
+'''
+inventario de un supermercado 
+registrar compras (suman productos en el inventario), 
+ventas (restan al inventario de productos), 
+
+2 opciones una de compra y otra de ventas, 
+
+validar que no se vendan productos que no tengan existencias 
+
+registrar los datos del producto código, nombre, existencias, precio unitario.
+'''
+import os
+
+control = True
+
+
+os.system('cls')
+while (control == True):
+    #menu
+    print(f'\t.:Gestion inventario menesex:.\n{}')
+    
+    opcion1 = int(input('[1]Registrar compras\n[2]Registrar ventas'))
+    if opcion1 == 1:
+        
+        
+    
+        
+            
+    
